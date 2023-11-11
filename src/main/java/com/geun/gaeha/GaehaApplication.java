aed4a6af603df3e9daf28bb569af34e854ebc944
geunnseung <bsg1017@naver.com>
2023-11-12 03:57:08 +0900

    :tada: gaeha 프로젝트 세팅

---
+++ src/main/java/com/geun/gaeha/GaehaApplication.java
@@ -0,0 +1,13 @@
+package com.geun.gaeha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaehaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaehaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geun/gaeha/GaehaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geun.gaeha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaehaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
